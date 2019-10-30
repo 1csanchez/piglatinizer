f712bcdd1dfe67531782d79bb000756cfa15b642
1csanchez <csanchez@aoiths.org>
2019-10-30 21:57:17 +0000

    piglatinazer

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ $( "document" ).ready(function() { //this helps your jQuery to work. Write all o
 $("button").click(function(){
 var inp=$("input").val();
 function convert(text){
+    var first=text[0];
 
-$("").html(text);
+$("").html(first);
 }
 convert(inp);
 });
